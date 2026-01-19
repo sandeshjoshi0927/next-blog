a8b9742d9e750c6c1309b1ce1f30ed4e1460f059
sandeshjoshi0927 <sandeshjoshi2211@gmail.com>
2026-01-19 22:51:44 +0545

    css

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Image from "next/image";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen">
+      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10  lg:justify-center lg:p-10 lg:py-0">
+        <div className="mb-16">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="logo"
+            width={224}
+            height={82}
+            className="h-auto w-[100px] lg:w-[150px]"
+          />
+        </div>
+        {children}
+      </section>
+    </div>
+  );
+};
+
+export default Layout;
