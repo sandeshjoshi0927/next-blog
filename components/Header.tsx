c53aca03d60d2555791f396acabbd9e2b6691cb2
sandeshjoshi0927 <sandeshjoshi2211@gmail.com>
2026-01-19 08:12:12 +0545

    navigation bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,34 @@
+"use client";
+
 import { navItems } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const path = usePathname();
+
+  const getActive = (url: string) => {
+    const activeClass = url === path ? "text-red-400" : "";
+    return activeClass;
+  };
+
   return (
-    <header className="">
+    <header className="flex items-center justify-between">
       <Image src="/assets/icons/logo.svg" width={64} height={24} alt="logo" />
 
-      <ul>
-        {navItems.map(({ name, url }) => (
-          <Link href={url} key={name}>
-            <li>{name}</li>
-          </Link>
-        ))}
-      </ul>
+      <nav className="bg-red-300">
+        <ul className="flex flex-1 gap-4 bg-green-50">
+          {navItems.map(({ name, url }) => (
+            <Link href={url} key={name}>
+              <li className={`text-light-100 px-6 ${getActive(url)}`}>
+                {name}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </nav>
 
       <Button type="submit" className="btn-primary">
         Sign In
